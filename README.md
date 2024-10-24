ee1630b1336a3098ccceb044d69fbaf2d04af924
Chori <yidodeveloper@gmail.com>
2024-10-25 00:46:05 +0900

    ⚡️ 모델 초기화 비용 절감

--- README.md
+++ README.md
@@ -4,7 +4,14 @@
 pip install fastapi uvicorn
 ```
 
-2. 웹 서버 실행
+2. Bert 모델 실행을 위한 패키지 설치
+
+``` shell
+pip install torch
+pip install transformers
+```
+
+3. 웹 서버 실행
 
 ``` shell
 uvicorn main:app --host 0.0.0.0 --port 8001
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from model import Item
-from evaluator import Evaluator
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+
+# 모델 파일 경로 설정
+base_dir = os.path.dirname(__file__)
+model_path = os.path.join(base_dir, 'model/career_maturity_classifier.pth')
 
 app = FastAPI()
 
@@ -14,8 +20,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+target_model = 'klue/bert-base'
+num_labels = 3  # 상, 중, 하를 3개의 클래스로 설정
+tokenizer = BertTokenizer.from_pretrained(target_model)
+model = BertForSequenceClassification.from_pretrained(target_model, num_labels=num_labels)
+model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+model.eval()  # 모델을 평가 모드로 설정
+
 @app.post('/grade')
 async def receive_data(item: Item):
-    evaluator = Evaluator()
-    grade = evaluator.predict(item.sentence)
-    return {'grade': grade}
+    inputs = tokenizer(item.sentence, return_tensors="pt", padding=True, max_length=512, truncation=True)
+    
+    with torch.no_grad():
+        outputs = model(**inputs)
+        prediction = torch.argmax(outputs.logits, dim=-1)
+
+    return {'grade': prediction.item() + 1}
